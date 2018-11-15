c6454094210bf27a21f73665a5724c1703d383f5
sleemans <s.leemans@bpm.rwth-aachen.de>
2018-11-15 07:02:06 +0000

    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveMiner/Trunk@40741 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveminer2/withoutlog/cutfinders/CutFinderWithoutLogIMLoopWithMinimumSelfDistance.java
+++ src/org/processmining/plugins/inductiveminer2/withoutlog/cutfinders/CutFinderWithoutLogIMLoopWithMinimumSelfDistance.java
@@ -1,6 +1,8 @@
 package org.processmining.plugins.inductiveminer2.withoutlog.cutfinders;
 
+import org.processmining.plugins.InductiveMiner.mining.cuts.Cut.Operator;
 import org.processmining.plugins.inductiveminer2.framework.cutfinders.Cut;
+import org.processmining.plugins.inductiveminer2.helperclasses.graphs.IntComponents;
 import org.processmining.plugins.inductiveminer2.withoutlog.MinerStateWithoutLog;
 import org.processmining.plugins.inductiveminer2.withoutlog.dfgmsd.DfgMsd;
 
@@ -8,14 +10,45 @@ public class CutFinderWithoutLogIMLoopWithMinimumSelfDistance implements CutFind
 
 	public Cut findCut(DfgMsd graph, MinerStateWithoutLog minerState) {
 
-		//first, check whether each activity has an outgoing msd-edge
+		/*
+		 * 1: all activities should have an outgoing msd-edge
+		 */
 		for (int node : graph.getActivities()) {
 			if (!graph.getMinimumSelfDistanceGraph().getOutgoingEdgesOf(node).iterator().hasNext()) {
 				return null;
 			}
 		}
 
-		return org.processmining.plugins.inductiveminer2.framework.cutfinders.CutFinderIMLoop.findCut(graph);
+		Cut cut = org.processmining.plugins.inductiveminer2.framework.cutfinders.CutFinderIMLoop.findCut(graph);
+		if (cut == null || !cut.isValid()) {
+			return null;
+		}
+
+		IntComponents components = new IntComponents(cut.getPartition());
+
+		//check the msd-footprint
+
+		/*
+		 * 4: redo-activities cannot have connections
+		 * 
+		 * We do this one first, as 2 and 3 might change when 4 is applied, but
+		 * 4 will not change when 2 or 3 is applied.
+		 */
+		{
+			for (long edgeIndex : graph.getMinimumSelfDistanceGraph().getEdges()) {
+				int source = graph.getMinimumSelfDistanceGraph().getEdgeSource(edgeIndex);
+				int target = graph.getMinimumSelfDistanceGraph().getEdgeTarget(edgeIndex);
+
+				int componentSource = components.getComponentOf(source);
+				int componentTarget = components.getComponentOf(target);
+
+				if (componentSource != 0 && componentTarget != 0 && componentSource != componentTarget) {
+					components.mergeComponentsOf(source, target);
+				}
+			}
+		}
+
+		return new Cut(Operator.loop, components.getComponents());
 	}
 
 }
