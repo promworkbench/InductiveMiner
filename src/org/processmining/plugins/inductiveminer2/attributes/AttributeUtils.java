c00e3e2891f71596ef6743decc8104ef2634f002
sleemans <s.leemans@bpm.rwth-aachen.de>
2020-08-13 04:22:59 +0000

    m
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveMiner/Trunk@43757 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/plugins/inductiveminer2/attributes/AttributeUtils.java
@@ -0,0 +1,85 @@
+package org.processmining.plugins.inductiveminer2.attributes;
+
+import java.text.DecimalFormatSymbols;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.deckfour.xes.model.XAttribute;
+import org.deckfour.xes.model.XAttributeContinuous;
+import org.deckfour.xes.model.XAttributeDiscrete;
+import org.deckfour.xes.model.XAttributeTimestamp;
+
+public class AttributeUtils {
+	public static long parseTimeFast(XAttribute attribute) {
+		if (attribute instanceof XAttributeTimestamp) {
+			return ((XAttributeTimestamp) attribute).getValueMillis();
+		}
+		return Long.MIN_VALUE;
+	}
+
+	/**
+	 * See if the given attribute has a numeric value. Returns -Double.MAX_VALUE
+	 * if not.
+	 * 
+	 * @param attribute
+	 * @return
+	 */
+	public static double parseDoubleFast(XAttribute attribute) {
+		if (attribute instanceof XAttributeDiscrete || attribute instanceof XAttributeContinuous) {
+			//the attribute was declared to be a number
+			if (attribute instanceof XAttributeDiscrete) {
+				return ((XAttributeDiscrete) attribute).getValue();
+			} else {
+				return ((XAttributeContinuous) attribute).getValue();
+			}
+		} else if (isStringNumeric(attribute.toString())) {
+			//the attribute was declared to be a string, check if it is not a number anyway
+			return NumberUtils.toDouble(attribute.toString(), -Double.MAX_VALUE);
+		}
+		return -Double.MAX_VALUE;
+	}
+
+	/**
+	 * See if the given attribute has a numeric value. Returns Long.MIN_VALUE if
+	 * not.
+	 * 
+	 * @param attribute
+	 * @return
+	 */
+	public static long parseLongFast(XAttribute attribute) {
+		if (attribute instanceof XAttributeDiscrete || attribute instanceof XAttributeContinuous) {
+			//the attribute was declared to be a number
+			if (attribute instanceof XAttributeDiscrete) {
+				return ((XAttributeDiscrete) attribute).getValue();
+			} else {
+				return (long) ((XAttributeContinuous) attribute).getValue();
+			}
+		} else if (isStringNumeric(attribute.toString())) {
+			//the attribute was declared to be a string, check if it is not a number anyway
+			return (long) NumberUtils.toDouble(attribute.toString(), Long.MIN_VALUE);
+		}
+		return Long.MIN_VALUE;
+	}
+
+	public static boolean isStringNumeric(String str) {
+		DecimalFormatSymbols currentLocaleSymbols = DecimalFormatSymbols.getInstance();
+		char localeMinusSign = currentLocaleSymbols.getMinusSign();
+
+		if (str.isEmpty() || !Character.isDigit(str.charAt(0)) && str.charAt(0) != localeMinusSign) {
+			return false;
+		}
+
+		boolean isDecimalSeparatorFound = false;
+		char localeDecimalSeparator = currentLocaleSymbols.getDecimalSeparator();
+
+		for (char c : str.substring(1).toCharArray()) {
+			if (!Character.isDigit(c)) {
+				if (c == localeDecimalSeparator && !isDecimalSeparatorFound) {
+					isDecimalSeparatorFound = true;
+					continue;
+				}
+				return false;
+			}
+		}
+		return true;
+	}
+}
